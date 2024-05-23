d5b74ff2cd2e27378ef707ee87bf1c388c3425cc
Michal Majer <Michal.Majer@StoneX.com>
2024-05-23 16:34:07 +0200

    Try to catch env.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { UniformContext } from '@uniformdev/canvas-next-rsc';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" data-catch-me="here-i-am">
       <body>
         <UniformContext>{children}</UniformContext>
       </body>
